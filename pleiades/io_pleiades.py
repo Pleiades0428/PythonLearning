0542d34d9b770b970ecddbc4717a82b95f071122
Pleiades <wangteng0428@msn.cn>
2017-07-29 00:18:50 +0800

    add io

---
+++ pleiades/io_pleiades.py
@@ -0,0 +1,12 @@
+#learn print
+print('Hello Python3 IO.')
+print('This', 'is', 'Python3')
+print('150 + 250 =', 100 + 200)
+
+#learn input
+print('What''s your name?')
+name = input()
+print('Hello,', name)
+
+#practice
+print('1024 * 768 =', 1024 * 768)
