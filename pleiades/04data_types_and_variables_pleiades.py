bc04af98f675a46d44107ea0d1271c5ba830ae7b
Pleiades <wangteng0428@msn.cn>
2017-08-02 22:52:26 +0800

    add data types and vars

---
+++ pleiades/04data_types_and_variables_pleiades.py
@@ -0,0 +1,66 @@
+# int
+print(1, 100, 1000)
+
+# hex
+print(0x01, 0x100, 0x1000)
+
+# float
+print(3.14, 5.2e8)
+
+# strings
+print('For honor!')
+print("I'm so good")
+print('I\'m so good!\tYes\n')
+
+# multi-lines
+print('''Hey I'm the 1st line.
+I'm the 2nd line
+I'm the 3rd line''')
+
+# with r
+print(r'''Hey I'm the 1st line.
+I'm the 2nd line
+I'm the 3rd line''')
+
+# bool
+print('I am the best' > '')
+print(24 > 15 and 3 != 4 or not (1 == 2 and 2 < 345))
+
+# None
+print(None)
+
+# variables
+a = 1
+Answer = True
+
+# assignment
+a = 'xxx'
+b = a
+a = 'yyy'
+print(b)
+
+# constant
+PI = 3.141592653
+
+# division
+print(10 / 3)
+# floor division
+print(10 // 3)
+# 
+print(10 % 3)
+
+
+# excercise
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
